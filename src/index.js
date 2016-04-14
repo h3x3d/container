47639c6a9fb4f7eeb4f2cb33af4df472a60ea9ec
h3x3d <h3x3d@ya.ru>
2016-04-14 17:38:25 +0300

    Built

---
+++ src/index.js
@@ -0,0 +1,44 @@
+export function Container() {
+  const _this = {};
+
+  const services = {};
+
+  const instances = new WeakMap();
+
+  function get(name, ...args) {
+    const key = [name, ...args];
+
+    if (!instances[key]) {
+      if (!services[name]) {
+        throw new Error(`UndefinedService ${name}`);
+      }
+
+      const inst = services[name].apply(null, args);
+
+      if (inst instanceof Promise) {
+        inst.then(res => instances[key] = res);
+      }
+
+      instances[key] = inst;
+    }
+
+    return instances[key];
+  }
+
+  function set(name, factory) {
+    services[name] = factory.bind(null, _this);
+  }
+
+  function factory(name) {
+    return services[name];
+  }
+
+  _this.get = get;
+  _this.set = set;
+  _this.factory = factory;
+
+  return _this;
+}
+
+export default Container();
+
